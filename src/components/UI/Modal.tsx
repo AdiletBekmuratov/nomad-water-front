0ade579c119e0824ec3438fec817b17252f84e63
“ARustamA” <rakhmetyanov@gmail.com>
2022-12-05 13:36:29 +0600

    измененная модалка

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -4,15 +4,17 @@ import { Button } from '../Forms';
 
 type Props = ForwardRefExoticComponent<RefAttributes<HTMLDivElement>> & {
   children: ReactNode;
-  title: string;
-  buttonOpenText: string;
-  buttonCloseTest: string;
+  title?: string;
+  buttonOpenText?: string;
+  buttonCloseTest?: string;
   buttonClassName?: string;
   titleClass?: string;
   contentClass?: string;
   isTwoButton?: boolean;
   secondButtonText?: string;
   openButtonStyle?: string;
+  className?: string;
+  closeButtonStyle?: string;
 };
 
 export const Modal: FC<Props> = (props) => {
@@ -21,45 +23,21 @@ export const Modal: FC<Props> = (props) => {
       <Dialog.Trigger asChild>
         <Button buttonColor={props.openButtonStyle}>{props.buttonOpenText}</Button>
       </Dialog.Trigger>
+
       <Dialog.Portal>
         <Dialog.Overlay className="fixed bg-black opacity-25 inset-0" />
         <Dialog.Content
-          className={`bg-white p-5 rounded-2xl z-10 ${
+          className={`bg-white p-5 sm:px-48 rounded-2xl z-10 ${
             props.contentClass ? props.contentClass : 'w-full'
-          } fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-screen-md`}>
-          <Dialog.Title
-            className={`text-center font-montserrat ${
-              props.titleClass ? props.titleClass : 'text-sm font-bold'
-            }`}>
-            {props.title}
-          </Dialog.Title>
-          <Dialog.Description
-            className={`font-montserrat ${props.className ? props.className : ''}`}>
+          } fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-screen-sm`}>
+          <div className={`flex flex-col gap-5`}>
             {props.children}
-          </Dialog.Description>
-          {props.isTwoButton ? (
-            <div className="w-full flex justify-around gap-2.5 mb-4">
+            {props.buttonCloseTest && (
               <Dialog.Close>
-                <Button
-                  className="w-40 md:w-56 lg:w-72"
-                  buttonColor="bg-gray-300 font-montserrat font-semibold"
-                  textColor="text-dark-blue">
-                  {props.buttonCloseTest}
-                </Button>
+                <Button>{props.buttonCloseTest}</Button>
               </Dialog.Close>
-              <Dialog.Close>
-                <Button
-                  className="w-40 md:w-56 lg:w-72"
-                  buttonColor="bg-dark-blue font-montserrat font-semibold">
-                  {props.secondButtonText}
-                </Button>
-              </Dialog.Close>
-            </div>
-          ) : (
-            <Dialog.Close>
-              <Button className="w-24">{props.buttonCloseTest}</Button>
-            </Dialog.Close>
-          )}
+            )}
+          </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
--- src/pages/NoAuthorizations.tsx
+++ src/pages/NoAuthorizations.tsx
@@ -3,22 +3,52 @@ import React from 'react';
 
 import googleplay from '../assets/crm/GooglePlay_en.png';
 import appstore from '../assets/crm/AppStore_en.png';
+import { Modal } from '@/components/UI';
+import { RegByPhone } from '@/components/regByPhone';
 
 export const NoAuthorizations = () => {
   return (
-    <div className={`grid grid-cols-1 gap-5 justify-center max-w-screen-sm mx-auto`}>
-      <span className={`text-base lg:text-lg font-semibold text-center`}>
+    <div className={`grid grid-cols-1 gap-5 justify-center max-w-screen-sm sm:px-36 mx-auto`}>
+      <span className={`text-base lg:text-lg font-semibold text-center `}>
         Вы не авторизованы! Пожалуйста, авторизуйтесь или зарегистрируйтесь!
       </span>
-      <Button>Зарегистрироваться</Button>
-      <Button>Войти</Button>
+      <Modal buttonOpenText="Зарегистрироваться" buttonCloseTest="Отмена">
+        <div className={`flex flex-col gap-3`}>
+          <span>Заполните все поля!</span>
+          <RegByPhone />
+        </div>
+      </Modal>
+
+      <Modal buttonOpenText="Войти" buttonCloseTest="Отмена">
+        <div className={`flex flex-col gap-3`}>
+          <span className={`text-lg font-semibold`}>Введите логин и пароль</span>
+          <Input
+            inputType="default"
+            id="phone"
+            name="phone"
+            label="Номер телефона"
+            mask="+7 (999) 999-99-99"
+            placeholder="+7 (999) 999-99-99"
+          />
+          <Input
+            inputType="default"
+            id="password"
+            name="password"
+            type={'password'}
+            label="Пароль"
+          />
+          <Button>Войти</Button>
+        </div>
+      </Modal>
       <p>Либо, скачайте мобильное приложение:</p>
       <div className="flex gap-5 justify-around">
-        <img src={googleplay} width={150} alt="googleplay" />
-        <img src={appstore} width={150} alt="appstore" />
+        <img src={googleplay} width={130} alt="googleplay" />
+        <img src={appstore} width={130} alt="appstore" />
       </div>
 
-      <span className={`text-base font-bold`}>Оставьте номер и мы Вам обязательно перезвоним!</span>
+      <span className={`text-base font-bold `}>
+        Оставьте номер и мы Вам обязательно перезвоним!
+      </span>
       <Input
         inputType="default"
         id="phone"
