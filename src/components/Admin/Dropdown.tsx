949009aa89b213b129e5474b8d47ef581f1e0b01
ALDIK <cjube1337@gmail.com>
2022-12-17 16:22:51 +0600

    logout

---
+++ src/components/Admin/Dropdown.tsx
@@ -0,0 +1,42 @@
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { logout } from '@/redux/slices/auth';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
+const Dropdown = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const handleLogout = async () => {
+    toast
+      .promise(dispatch(logout()).unwrap(), {
+        success: 'Вышли из аккаунта',
+        loading: 'Загрузка...',
+        error: (err) => err.toString()
+      })
+      .then(() => {
+        navigate('/admin');
+      });
+  };
+
+  return (
+    <div
+      className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-light-blue shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+      role="menu"
+      aria-orientation="vertical"
+      aria-labelledby="menu-button"
+      tabIndex={-1}>
+      <div className="" role="none">
+        <button
+          onClick={handleLogout}
+          className="text-gray-700 px-4 py-2 text-sm w-full flex justify-start hover:bg-white hover:rounded-md "
+          role="menuitem"
+          tabIndex={-1}
+          id="menu-item-0">
+          Выйти
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
