3c4596584b3d693f9be0609f2dd288ecdeb9752d
ALDIK <cjube1337@gmail.com>
2022-12-26 12:52:03 +0600

    Тесты с Богдана телефоном

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,19 +1,23 @@
-import { Button, FormContainer, Input } from '@/components/Forms';
+import { Button, Input } from '@/components/Forms';
 // import { Modal } from '@/components/Layout/Modal';
 // import { useAppSelector } from '@/hooks/useAppSelector';
-import { IUserFull } from '@/types';
+import { ILoginForm, IUserFull } from '@/types';
 import { Form, Formik } from 'formik';
 // import { useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 import { login } from '@/redux/slices/auth';
 
 import * as Yup from 'yup';
-import { useCreateUserAccountMutation } from '@/redux/services/base.service';
-import Checkbox from '@/components/Checkbox';
-import { useState } from 'react';
+import {
+  useCreateUserAccountMutation,
+  useGetUserCodeMutation
+} from '@/redux/services/base.service';
+import { useEffect, useState } from 'react';
 import { Modal } from '@/components/Layout/Modal';
 import { useAppDispatch } from '@/hooks/useAppDispatch';
 import { useNavigate } from 'react-router-dom';
+import Checkbox from '@/components/Checkbox';
+import { useAppSelector } from '@/hooks/useAppSelector';
 
 const SignInSchema = Yup.object().shape({
   phone: Yup.string().required('Обязательное поле для заполнения')
@@ -33,13 +37,40 @@ const INIT: IUserFull = {
   houseNumber: ''
 };
 
+const initial: ILoginForm = {
+  password: '',
+  phone: ''
+};
+
 const LoginPage = () => {
   const [createAccount, { isLoading }] = useCreateUserAccountMutation();
   const navigate = useNavigate();
+  const [genCode, { isLoading: codeLoading }] = useGetUserCodeMutation();
+
+  const { user } = useAppSelector((state) => state.auth);
 
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [phoneNumb, setPhoneNumb] = useState('');
+  const [isPhone, setIsPhone] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login/user');
+    } else {
+      navigate('/');
+    }
+  }, [user]);
+
   const dispatch = useAppDispatch();
+  const generateCode = async (values) => {
+    console.log(values);
+    toast.promise(genCode(values).unwrap(), {
+      loading: 'Загрузка...',
+      success: 'Код успешно отправлен',
+      error: (error) => JSON.stringify(error.data, null, 2)
+    });
+  };
+
   const handleCreate = async (values: IUserFull) => {
     console.log(values);
     setPhoneNumb(values.phone);
@@ -89,91 +120,114 @@ const LoginPage = () => {
             <div className="text-center">
               <h2 className="mt-6 text-3xl font-bold text-gray-900">Добро пожаловать!</h2>
             </div>
-
-            <Formik initialValues={INIT} onSubmit={handleCreate} validationSchema={SignInSchema}>
-              {() => (
-                <Form className="mt-8 space-y-6 grid grid-cols-3 gap-1">
-                  <div>
+            <Checkbox
+              label="Зарегистрировать по номеру телефона"
+              onChange={() => setIsPhone(!isPhone)}
+            />
+            {!isPhone ? (
+              <Formik initialValues={INIT} onSubmit={handleCreate} validationSchema={SignInSchema}>
+                {() => (
+                  <Form className="mt-8 space-y-6 grid grid-cols-3 gap-1">
+                    <div>
+                      <Input
+                        inputType="formik"
+                        id="firstname"
+                        name="firstname"
+                        label="Имя"
+                        width="w-full"
+                      />
+                    </div>
+                    <div>
+                      <Input
+                        inputType="formik"
+                        id="lastname"
+                        name="lastname"
+                        label="Фамилия"
+                        width="w-full"
+                      />
+                    </div>
+                    <div>
+                      <Input
+                        inputType="formik"
+                        id="middleName"
+                        name="middleName"
+                        label="Отчество"
+                        width="w-full"
+                      />
+                    </div>
+                    <div className="col-span-3"></div>
+                    <div>
+                      <Input
+                        inputType="formik"
+                        id="street"
+                        name="street"
+                        label="Улица"
+                        width="w-full"
+                      />
+                    </div>
+                    <div>
+                      <Input
+                        inputType="formik"
+                        id="houseNumber"
+                        name="houseNumber"
+                        label="Номер дома"
+                        width="w-full"
+                      />
+                    </div>
+                    <div>
+                      <Input
+                        inputType="formik"
+                        id="flat"
+                        name="flat"
+                        label="Квартира"
+                        width="w-full"
+                      />
+                    </div>
+                    <div className="col-span-3">
+                      <Input
+                        inputType="formik"
+                        id="birthday"
+                        name="birthday"
+                        label="Дата рождения"
+                        width="w-full"
+                        type="date"
+                      />
+                    </div>
+                    <div className="col-span-3">
+                      <Input
+                        inputType="formik"
+                        id="phone"
+                        name="phone"
+                        label="Номер телефона"
+                        width="w-full"
+                        mask="+7 (999) 999 99 99"
+                      />
+                    </div>
+                    <Button type="submit" loading={isLoading} className="col-span-3">
+                      Создать аккаунт
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+            ) : (
+              <>
+                <Formik initialValues={{ phone: '' }} onSubmit={generateCode}>
+                  <Form>
                     <Input
                       inputType="formik"
-                      id="firstname"
-                      name="firstname"
-                      label="Имя"
-                      width="w-full"
-                    />
-                  </div>
-                  <div>
-                    <Input
-                      inputType="formik"
-                      id="lastname"
-                      name="lastname"
-                      label="Фамилия"
-                      width="w-full"
-                    />
-                  </div>
-                  <div>
-                    <Input
-                      inputType="formik"
-                      id="middleName"
-                      name="middleName"
-                      label="Отчество"
-                      width="w-full"
-                    />
-                  </div>
-                  <div className="col-span-3"></div>
-                  <div>
-                    <Input
-                      inputType="formik"
-                      id="street"
-                      name="street"
-                      label="Улица"
-                      width="w-full"
-                    />
-                  </div>
-                  <div>
-                    <Input
-                      inputType="formik"
-                      id="houseNumber"
-                      name="houseNumber"
-                      label="Номер дома"
-                      width="w-full"
-                    />
-                  </div>
-                  <div>
-                    <Input
-                      inputType="formik"
-                      id="flat"
-                      name="flat"
-                      label="Квартира"
-                      width="w-full"
-                    />
-                  </div>
-                  <div className="col-span-3">
-                    <Input
-                      inputType="formik"
-                      id="birthday"
-                      name="birthday"
-                      label="Дата рождения"
-                      width="w-full"
-                      type="date"
-                    />
-                  </div>
-                  <div className="col-span-3">
-                    <Input
-                      inputType="formik"
-                      id="phone"
                       name="phone"
-                      label="Номер телефона"
-                      width="w-full"
+                      id="phone"
                       mask="+7 (999) 999 99 99"
+                      className="mb-2"
+                      placeholder="+7 (999) 999 99 99"
                     />
-                  </div>
-                  <Button type="submit" loading={isLoading} className="col-span-3">
-                    Создать аккаунт
-                  </Button>
-                </Form>
-              )}
-            </Formik>
+                    <Button type="submit" loading={codeLoading}>
+                      Отправить код
+                    </Button>
+                  </Form>
+                </Formik>
+              </>
+            )}
           </div>
         </div>
       </div>
@@ -184,22 +238,20 @@ const LoginPage = () => {
         <p className="text-center font-montserrat text-gray-700 text-sm">
           Код придет вам в течении одной минуты
         </p>
-        <FormContainer
-          className="p-4"
-          initialValues={{ password: '', phone: '' }}
-          validationSchema={validation}
-          buttonName={`Войти в аккаунт`}
-          buttonAction={handleSubmit}>
-          <div className="mb-3">
-            <Input
-              inputType="formik"
-              name="password"
-              id="password"
-              type="password"
-              label="Код подтверждения"
-            />
-          </div>
-        </FormContainer>
+        <Formik initialValues={initial} validationSchema={validation} onSubmit={handleSubmit}>
+          <Form>
+            <div className="mb-3">
+              <Input
+                inputType="formik"
+                name="password"
+                id="password"
+                type="password"
+                label="Код подтверждения"
+              />
+            </div>
+            <Button>Отправить</Button>
+          </Form>
+        </Formik>
       </Modal>
     </section>
   );
