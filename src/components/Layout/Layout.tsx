2ccaefc00ddf7a9d10bd0ca300144869e29213c1
ALDIK <cjube1337@gmail.com>
2023-01-07 15:32:03 +0600

    layout fix

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -10,10 +10,12 @@ interface ILayoutProps extends HTMLAttributes<HTMLDivElement> {
 export const Layout: FC<ILayoutProps> = ({ children, ...props }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="max-h-full overflow-y-hidden">
+    <div className="max-h-screen h-screen min-h-screen overflow-y-hidden">
       <Sheet isOpen={isOpen} setIsOpen={setIsOpen}>
         <Header setIsOpen={setIsOpen} />
-        <div {...props} className={`layout py-4 md:py-8 mb-20 md:mb-0 w-full  ${props.className}`}>
+        <div
+          {...props}
+          className={`layout py-4 md:py-8 mb-20 md:mb-0 w-full h-screen ${props.className}`}>
           {children}
         </div>
         <MenuBottom />
